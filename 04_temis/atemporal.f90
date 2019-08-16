e5860ee21d08145c68ef54a67204ef0e45e6c318
Agustin Garcia-Reynoso <agustin@atmosfera.unam.mx>
2019-08-16 16:20:28 -0500

    Point emissions update  Changes to be committed: 	renamed:    07_puntual/Puntual2013.csv -> 07_puntual/Puntuales2016.csv 	modified:   07_puntual/puntual.csv

--- 04_temis/atemporal.f90
+++ 04_temis/atemporal.f90
@@ -476,6 +476,7 @@ print *,"   Compute  VOCs",size(idcel2)
 subroutine storage
   implicit none
   integer i,j,k,l
+  real suma
   character(len=3):: cdia(7)
   data cdia/'MON','TUE','WND','THR','FRD','SAT','SUN'/
  print *,"Storage"
@@ -484,7 +485,11 @@ subroutine storage
    write(10,*)casn(k),'ID, Hr to Hr24'
    write(10,'(I8,4A)')size(emis,dim=1),",",current_date,', ',cdia(daytype)
    do i=1,size(emis,dim=1)
-     write(10,100)idcel2(i),(emis(i,k,l),l=1,nh)
+     suma=0
+     do l=1,nh
+       suma=suma+emis(i,k,l)
+     end do
+     if(suma.gt.0) write(10,100)idcel2(i),(emis(i,k,l),l=1,nh)
    end do
    close(unit=10)
   end do
@@ -497,7 +502,11 @@ subroutine storage
    write(10,'(I8,4A)')size(epm2,dim=1)*nscc(k),",",current_date,', ',cdia(daytype)
    do i=1,size(epm2,dim=1)
      do j=1,nscc(k)
-     write(10,110)idcel2(i),iscc(j),(epm2(i,j,l),l=1,nh)
+     suma=0
+     do l=1,nh
+       suma=suma+epm2(i,j,l)
+     end do
+     if(suma.gt.0)  write(10,110)idcel2(i),iscc(j),(epm2(i,j,l),l=1,nh)
      end do
    end do
 	close(10)
@@ -509,7 +518,11 @@ subroutine storage
    write(10,'(I8,4A)')size(evoc,dim=1)*nscc(k),",",current_date,', ',cdia(daytype)
    do i=1,size(evoc,dim=1)
      do j=1,nscc(k)
-     write(10,110)idcel2(i),iscc(j),(evoc(i,j,l),l=1,nh)
+     suma=0
+     do l=1,nh
+       suma=suma+evoc(i,j,l)
+     end do
+     if(suma.gt.0)  write(10,110)idcel2(i),iscc(j),(evoc(i,j,l),l=1,nh)
      end do
    end do
 	close(10)
