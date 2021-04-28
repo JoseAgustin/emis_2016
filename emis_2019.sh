f31215893a568bb1a7c2e81c78952aa5c873fa6d
Agustin Garcia <agustin@atmosfera.unam.mx>
2021-04-27 22:21:03 -0500

    autotest

--- emis_2019.sh
+++ emis_2019.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
+#
 #: Title       : emis_2019.sh
-#: Date        : 26/07/2017
+#: Date        : 25/04/2021
 #: Author      : "Jose Agustin Garcia Reynoso" <agustin@atmosfera.unam.mx>
-#: Version     : 1.1  26/04/2020 Actualizacion para IE del 2016
-#: Description : Realiza la secuencia de pasos para generar diferentes fechas
-#                del inventario de emisiones.
+#: Version     : 1.0  26/04/2021 Actualizacion para IE del 2016
+#: Description : Programa de emisiones con funciones
 #: Options     : None
-#
-# bsub -q q_hpc -oo salida_postwrf -n2 -R "span[hosts=1]" './febrero_2019.csh'
-#
 #SBATCH -J emi_2019
 #SBATCH -o emi_2019%j.o
 #SBATCH -n 4
 #SBATCH --ntasks-per-node=24
 #SBATCH -p id
-ProcessDir=$PWD
-echo "Directorio actual "$ProcessDir
-# Selecciona area de modelacion
-# bajio bajio3 cdjuarez   colima    ecacor  ecaim ecaim3
-# guadalajara  jalisco    mexicali  mexico  mexico9
-# monterrey    monterrey3 queretaro tijuana
+# Emissions area avalable:
+#   bajio bajio3 cdjuarez   colima    ecacor  ecaim ecaim3
+#   guadalajara  jalisco    mexicali  mexico  mexico9
+#   monterrey    monterrey3 queretaro tijuana
 #
 dominio=tijuana
-HacerArea=1
+# To set spatial distribution = 1 else =0
+HacerArea=0
 #
-# Selecciona mecanismo
-# Los mecanismos a usar:
+# Mechanism selcetion
+# avalable:
 # cbm04 cbm05 mozart racm2 radm2 saprc99 saprc07
 #
 MECHA=radm2
@@ -34,9 +30,8 @@ AQM_SELECT=1
 #  Build the namelist_emis.nml file
 # Cambiar aqui la fecha
 mes=5
-dia=9
-dia2=9
-dia1=$dia 
+dia=11
+dia2=12
 #
 #    Aqui cambiar el año a modelar
 #
@@ -44,175 +39,54 @@ nyear=2019
 #
 #   Si se desea un archivo de 24 hrs  nfile=1
 #              dos archivos de 12 hrs nfile=2
-nfile=1
-#  Revisa que exista el dominio
-cd 01_datos
-existe=0
-for dir in $(ls)
- do
-if [ "$dir" = "$dominio" ]; then
-  existe=1
-fi
-done
-cd ..
-if [ $existe -eq 0 ];then
-echo "  Dominio "$dominio" no existe en el directorio *****"
-exit
-else
-echo "  Relizando para "$dominio"   *****"
-cat << End_Of_File > namelist_emis.nml
-!
-!   Definicion de variables para calculo del Inventario
-!
-&region_nml
-zona ="$dominio"
-!
-! bajio bajio3 cdjuarez   colima    ecacor  ecaim ecaim3
-! guadalajara  jalisco    mexicali  mexico  mexico9
-! monterrey    monterrey3 queretaro tijuana
-/
-&fecha_nml
-! Se indica el dia y el mes a calcular
-! se proporciona el anio
-! month jan =1 to dec=12
-! day in the month (from 1 to 28,30 or 31)
-! anio a modelar validos: 2014 a 2020
-! Si se quiere un archvio de 24 hr periodo=1
-!    o dos archivos de 12 hr peridodo=2
-idia=$dia
-month=$mes
-anio=$nyear
-periodo=$nfile
-/
-!Horario de verano
-&verano_nml
-! .true. o .false. para considerar o no el cambio cuando se
-!  esta en horario de verano
-!
-lsummer = .true.
-/
-! Quimica a utilizar
-! Los mecanismos a usar:
-!  cbm04 cbm05 mozart racm2 radm2 saprc99 saprc07
-!  if saprc07  model= 0 WRF 1 CHIMERE
-&chem_nml
-mecha='$MECHA'
-model=$AQM_SELECT
-/
-End_Of_File
+nfile=2
 
-cd 02_aemis
-if [ $HacerArea -eq 1 ]; then
-echo "     Haciendo distribucion espacial en Fuentes de area"
-./ASpatial.exe >../area.log
-fi
-cd ../03_movilspatial
-if [ $HacerArea -eq 1 ]; then
-echo "     Haciendo distribucion espacial en Fuentes de Moviles"
-./vial.exe > ../movil.log &
-./carr.exe >> ../movil.log&
-wait
-./agrega.exe > ../movil.log 
-cd ../05_semisM
-./MSpatial.exe > ../movil.log &
-wait
-fi
+#####  END OF USER MODIFICATIONS  #####
+source functions.sh
 
-fi
+check_domain $dominio
+
+make_tmpdir tmp$dominio
+
+hace_namelist $dia $dia
+
+hace_area &
+
+hace_movil
+
+wait
+
 #
-# Inicia Loop de Tiempo
-# exit
+# Starts Loop for Time
+#
 while [ $dia -le $dia2 ] ;do
-cd $ProcessDir
-echo "directorio de trabajo "$PWD
-echo ' '
+if [ -d dia$dia ]
+then
+   cd dia$dia
+else
+   mkdir dia$dia
+   cd dia$dia
+fi
+hace_namelist $dia -$dia
+echo "Working Directory "$PWD
 echo '  Mes ='$mes 'DIA '$dia
-#
-cat << End_Of_File > namelist_emis.nml
-!
-!   Definicion de variables para calculo del Inventario
-!
-&region_nml
-zona ="$dominio"
-!
-! bajio bajio3 cdjuarez   colima    ecacor  ecaim ecaim3
-! guadalajara  jalisco    mexicali  mexico  mexico9
-! monterrey    monterrey3 queretaro tijuana
-/
-&fecha_nml
-! Se indica el dia y el mes a calcular
-! se proporciona el anio
-! month jan =1 to dec=12
-! day in the month (from 1 to 28,30 or 31)
-! anio a modelar validos: 2014 a 2020
-! Si se quiere un archvio de 24 hr periodo=1
-!    o dos archivos de 12 hr peridodo=2
-idia=$dia
-month=$mes
-anio=$nyear
-periodo=$nfile
-/
-!Horario de verano
-&verano_nml
-! .true. o .false. para considerar o no el cambio cuando se
-!  esta en horario de verano
-!
-lsummer = .true.
-/
-! Quimica a utilizar
-! Los mecanismos a usar:
-!  cbm04 cbm05 mozart racm2 radm2 saprc99 saprc07
-!  IF saprc07  model= 0 WRF 1 CHIMERE
-&chem_nml
-mecha='$MECHA'
-model=$AQM_SELECT
-/
-End_Of_File
+emis_area &
 
-cd $ProcessDir/04_temis
-echo 'Area Temporal distribution'
-./Atemporal.exe  > ../area.log &
-echo 'Point Temporal distribution'
-cd ../07_puntual/
-./Puntual.exe > ../puntual.log &
-echo 'Movil Temporal distribution'
-cd ../06_temisM/
-./Mtemporal.exe > ../movil.log &
-wait
-#
-#echo 'Biogenic'
-#cd ../12_biogenic
-#./Btemporal.exe > biog.log&
-#
-echo 'Speciation distribution PM2.5'
-#
-cd ../09_pm25spec
-./spm25p.exe >> ../puntual.log &
-./spm25m.exe >> ../movil.log &
-./spm25a.exe >> ../area.log&
-#
+emis_fijas &
 
-echo 'Speciation distribution VOCs'
-#
-cd ../08_spec
-echo '   '$MECHA' *****'
-echo 'Movile'
-./spm.exe >> ../movil.log &
-echo 'Puntual'
-./spp.exe >> ../puntual.log  &
-echo 'Area '
-./spa.exe >> ../area.log
+emis_movil &
+
 wait
 
-echo ' Guarda'
+ln -fs ../chem/namelist.* .
+../bin/emiss.exe  > ../${MECHA}.log
+if [ ! -d ../../inventario/${dominio} ]
+  then
+    mkdir -p ../../inventario/${dominio}
+fi
+    mv *00\:00 ../../inventario/${dominio}
 
-cd ../10_storage
-./emis2.exe  > ../${MECHA}.log
+cd ..
+echo "DONE  Guarda "$MECHA $dia
  let dia=dia+1
 done
-#mv wrfchemi?d01* ../inventario/$dominio/
-#ncrcat -O wrfchemi.d01.radm2.2019-0${mes}-1* wrfchemi_d01_2019-0${mes}-${dia}_00:00:00
-#mv wrfchemi_d01_2019-02-05_00:00:00 ../../DOMAIN/mecanismos/emisiones
-echo "DONE  Guarda "$MECHA
-exit
-
