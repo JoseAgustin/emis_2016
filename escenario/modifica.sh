fa09fb6cf4e3893af673f64f4747ab66454f8ae5
Agustin Garcia-Reynoso <agustin@atmosfera.unam.mx>
2022-10-14 09:33:20 -0500

    Update Toluca base case scenario

--- escenario/modifica.sh
+++ escenario/modifica.sh
@@ -8,8 +8,9 @@
 #: Options     : None
 #
 ProcessDir=$PWD
-echo "Directorio actual "$ProcessDir
-echo "Emisiones de fuentes móviles"
+echo "------"
+echo "   Directorio actual "$ProcessDir
+echo "   Emisiones de fuentes móviles"
 #awk ' NR>1 {print $1}' municipio.txt
 # Evalua si existe el archivo de salida para borrar
 if [ -e tol_base_movil.csv ]
@@ -35,55 +36,10 @@ if ($1==15005 || $1==15018 || $1==15027 || $1==15051 || $1==15054 ||
   printf ("\n");
 }else
 	print $0;
-}' $IEMA  >> tol_base_movil.csv
-#
-echo Emisiones de VOC fuentes de área x2.4
-IEMA=../01_datos/emis/area/IVOC_2016.csv
-head -3 $IEMA > tol_base_IVOC.csv
-awk -F, 'NR>3 {
-if ($3==15005 || $3==15018 || $3==15027 || $3==15051 || $3==15054 ||
-    $3==15055 || $3==15062 || $3==15067 || $3==15072 || $3==15073 ||
-    $3==15076 || $3==15087 || $3==15090 || $3==15106 || $3==15115 || $3==15118)
-    {
-      printf ("%s,%s,%s,",$1,$2,$3) ;
-      for (i = 4; i < NF; i++) printf ("%g," $i*2.4) ;
-      printf("%f" $NF*2.4);
-    }
-    else print $0}' $IEMA >>tol_base_IVOC.csv
-#
-echo Emisiones de SO2 fuentes de área x10
-IEMA=../01_datos/emis/area/ISO2_2016.csv
-head -3 $IEMA > tol_base_ISO2.csv
-awk -F, 'NR>3 {
-if ($1==5 && ( $2==18 || $2==27 || $2==51 || $2==54 ||
-     $2==55 || $2==62 || $2==67 || $2==72 || $2==73 ||
-     $2==76 || $2==87 || $2==90 || $1==106||$2==115 || $2==118))
-    {
-      printf ("%s,%s,%s,",$1,$2,$3) ;
-      for (i = 4; i < NF; i++) printf("%f," $i*10) ;
-      printf("%f" $NF*10);
-    }
-    else print $0 }' $IEMA >> tol_base_ISO2.csv
-#
-echo Emisiones de CO fuentes de área x4
-IEMA=../01_datos/emis/area/ICO__2016.csv
-head -3 $IEMA > tol_base_ICO.csv
-awk -F, 'NR>3 {
-if ($1==5 && ( $2==18 || $2==27 || $2==51 || $2==54 ||
-     $2==55 || $2==62 || $2==67 || $2==72 || $2==73 ||
-     $2==76 || $2==87 || $2==90 || $1==106||$2==115 || $2==118))
-    {
-      printf ("%s,%s,%s,",$1,$2,$3) ;
-      for (i = 4; i < NF; i++) printf("%f," $i*4) ;
-      printf("%f" $NF*4);
-    }
-    else  print $0}' $IEMA >> tol_base_ICO.csv
-    
-# Evalua si existe el archivo de salida para borrar
-if [ -e tol_baseI.csv ]
-then
-rm tol_escII.csv
-fi
+}' $IEMI  >> tol_base_movil.csv
+IEMI=tol_base_movil.csv
+#  Escenario 2
+printf "\t Escenario 2 fuentes Moviles\n"
 head -1 $IEMI > tol_escII.csv
 # Escenario 2
 # VOC     CO    NOx    NO2    NH3    SO2    PM10  PM2.5  CO2     CH4   CN
@@ -93,14 +49,62 @@ head -1 $IEMI > tol_escII.csv
 awk -F, 'NR>1 {
 if ($1==15005 || $1==15018 || $1==15027 || $1==15051 || $1==15054 ||
     $1==15055 || $1==15062 || $1==15067 || $1==15072 || $1==15073 ||
-    $1==15076 || $1==15087 || $1==15090 || $1==15106 || $1==15115 || $1==15118){
-  printf ("%s,%s,", $1, $2) ;
-  printf("%f,%f,%f,%f,", $3*0.7921, $4*0.8139, $5*0.8506, $6*0.8529);
-  printf("%f,%f,%f,%f,", $7*0.8060, $8*0.7915, $9*0.7988,$10*0.8551);
-  printf("%f,%f,%f,",$11*0.8253,$12*0.8213,$13*0.8435);
-  printf ("%s",$NF)
-  printf ("\n");
+    $1==15076 || $1==15087 || $1==15090 || $1==15106 || $1==15115 || $1==15118)
+{
+printf ("%s,%s,", $1, $2) ;
+printf("%f,%f,%f,%f,", $3*0.7921, $4*0.8139, $5*0.8506, $6*0.8529);
+printf("%f,%f,%f,%f,", $7*0.8060, $8*0.7915, $9*0.7988,$10*0.8551);
+printf("%f,%f,%f,",$11*0.8253,$12*0.8213,$13*0.8435);
+printf ("%s\n",$NF);
 }else
-  print $0;
+print $0 ;
 }' $IEMI  >> tol_escII.csv
+#
+echo Emisiones de VOC fuentes de área x2.4
+IEMI=../01_datos/emis/area/IVOC_2016.csv
+head -3 $IEMI > tol_base_IVOC.csv
+awk -F, 'NR>3 {
+if ($3==15005 || $3==15018 || $3==15027 || $3==15051 || $3==15054 ||
+    $3==15055 || $3==15062 || $3==15067 || $3==15072 || $3==15073 ||
+    $3==15076 || $3==15087 || $3==15090 || $3==15106 || $3==15115 || $3==15118)
+    {
+      printf ("%s,%s,%s,",$1,$2,$3) ;
+      for (i = 4; i < NF; i++) printf ("%g," ,$i*2.4) ;
+      printf("%f\n", $NF*2.4);
+    }
+    else print $0}' $IEMI >>tol_base_IVOC.csv
+#
+echo Emisiones de SO2 fuentes de área x10
+IEMI=../01_datos/emis/area/ISO2_2016.csv
+head -3 $IEMI > tol_base_ISO2.csv
+awk -F, 'NR>3 {
+if ($3==15005 || $3==15018 || $3==15027 || $3==15051 || $3==15054 ||
+    $3==15055 || $3==15062 || $3==15067 || $3==15072 || $3==15073 ||
+    $3==15076 || $3==15087 || $3==15090 || $3==15106 || $3==15115 || $3==15118)
+    {
+      printf ("%s,%s,%s,",$1,$2,$3) ;
+      for (i = 4; i < NF; i++) printf("%f," ,$i*10) ;
+      printf("%f\n", $NF*10);
+    }
+    else print $0 }' $IEMI >> tol_base_ISO2.csv
+#
+echo Emisiones de CO fuentes de área x4
+IEMI=../01_datos/emis/area/ICO__2016.csv
+head -3 $IEMI > tol_base_ICO.csv
+awk -F, 'NR>3 {
+if ($3==15005 || $3==15018 || $3==15027 || $3==15051 || $3==15054 ||
+    $3==15055 || $3==15062 || $3==15067 || $3==15072 || $3==15073 ||
+    $3==15076 || $3==15087 || $3==15090 || $3==15106 || $3==15115 || $3==15118)
+    {
+      printf ("%s,%s,%s,",$1,$2,$3) ;
+      for (i = 4; i < NF; i++) printf("%f,", $i*4) ;
+      printf("%f\n", $NF*4);
+    }
+    else  print $0 }' $IEMI >> tol_base_ICO.csv
+    
+# Evalua si existe el archivo de salida para borrar
+if [ -e tol_baseI.csv ]
+then
+rm tol_escII.csv
+fi
 echo "TERMINA"
