2b7c468f2c39c8fb4a53500686039e5237155810
Agustin <agustin@atmosfera.unam.mx>
2023-10-07 15:59:55 -0600

    Add speciation

---
+++ util/especiacion.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#: Title       :Suma especies quimicas
+#: Date        : 10-7-2023
+#: Author      : "Agustin Garcia"<agustin@atmosfera.unam.mx>
+#: Version     : 1.0
+#: Description : Obtain the total emissions fros speciation
+#: Options     : None
+
+# avalable:
+# cbm04 cbm05 mozart racm2 radm2 saprc99 saprc07 ghg
+MECHA=RADM
+spec=(${MECHA}*_M.txt)
+num=${#spec[@]}
+for (( i=1; i<$num ;i++))
+do
+ printf "%s"  "${spec[$i]:7:4}, "
+ awk -F"," -f ../../util/suma_spc.awk ${spec[$i]}
+done
