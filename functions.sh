14448d104751d8e3b01ef64509a00fe43b2c1a49
Agustin <agustin@atmosfera.unam.mx>
2024-01-18 09:52:00 -0600

    Update for sunday functions
    Sunday from date is 0 now is 7 for this scritp

--- functions.sh
+++ functions.sh
@@ -1,91 +1,57 @@
 #!/bin/bash
 #
 #: Title       : functions.sh
-#: Date        : 25/04/2021
+#: Date        : 02/01/2024
 #: Author      : "Jose Agustin Garcia Reynoso" <agustin@atmosfera.unam.mx>
 #: Version     : 1.0  26/04/2021 Actualizacion para IE del 2016
 #: Description : Functios to run emissions
 #: Options     : None
 
-#: Title       : hace_namelist
-#: Date        : 25/04/2021
+#: Title       : crea_anio_csv()
+#: Date        : 2/01/2024
 #: Author      : "Jose Agustin Garcia Reynoso" <agustin@atmosfera.unam.mx>
-#: Version     : 1.0  26/04/2021 Actualizacion para IE del 2016
-#: Description : generates the namelist.nml
-#: Options     : $dia $dia2
-hace_namelist()
+#: Version     : 1.0   2/01/2024
+#: Description : Crea el archivo de anio con base en la fecha
+#: Options     : recibe fecha y la convierte  anio, mes dia
+#
+crea_anio_csv()
 {
- if [ $1 -eq $2 ]
- then
-  echo  "  *** Creating namelist.nml for "$dominio"   *****"
- else
-  echo  "  *** Creating namelist.nml for day "$1"   *****"
- fi
-cat << End_Of_File > namelist_emis.nml
-!
-!   Definicion de variables para calculo del Inventario
-!
-&region_nml
-zona ="$dominio"
-!
-! bajio bajio3 cdjuarez   colima    ecacor  ecaim ecaim3
-! guadalajara  jalisco    mexicali  mexico  mexico9
-! monterrey    monterrey3 queretaro tijuana
-/
-&fecha_nml
-! Se indica el dia y el mes a calcular
-! se proporciona el anio
-! month jan =1 to dec=12
-! day in the month (from 1 to 28,30 or 31)
-! anio a modelar validos: 2014 a 2022
-! Si se quiere un archvio de 24 hr periodo=1
-!    o dos archivos de 12 hr peridodo=2
-idia=$dia
-month=$mes
-anio=$nyear
-periodo=$nfile
-/
-!Horario de verano
-&verano_nml
-! .true. o .false. para considerar o no el cambio cuando se
-!  esta en horario de verano
-!
-lsummer = .false.
-/
-! Quimica a utilizar
-! Los mecanismos a usar:
-!  cbm04 cbm05 mozart racm2 radm2 saprc99 saprc07
-!  if saprc07  model= 0 WRF 1 CHIMERE
-&chem_nml
-mecha='$MECHA'
-model=$AQM_SELECT
-/
-End_Of_File
-
-}
-#: Title       : check_domain
-#: Date        : 25/04/2021
-#: Author      : "Jose Agustin Garcia Reynoso" <agustin@atmosfera.unam.mx>
-#: Version     : 1.0  26/04/2021 Actualizacion para IE del 2016
-#: Description : Identifies if the domain are for emissions exists
-#: Options     : domain
-check_domain ()
-{
-echo -e "      \033[3;46m   ___  _ ___ _____ ___   \033[0m"
-echo -e "      \033[3;46m  |   \(_) __|_   _| __|  \033[0m"
-echo -e "      \033[3;46m  | |) | | _|  | | | _|   \033[0m"
-echo -e "      \033[3;46m  |___/|_|___| |_| |___|  \033[0m"
-echo -e "      \033[3;46m                          \033[0m"
-cd 01_datos
-existe=0
-if [ -d $1 ]
+if [ $# -eq 0 ];
 then
-   echo "  *** Realizando para "$dominio"   *****"
+   export anio1=`date +%Y`
+   linea=$(date +%m,%d,%w,%a)
+   echo "**** sin argumentos  *****"
+   echo Se realizara para hoy $anio1 $linea 
 else
-   echo -e "\033[1;45m  Dominio "$dominio" no existe en el directorio \033[0m"
-   exit 1
+   echo Con argumentos
+   if [[ $1 -gt 1000 && $1 -le 2050 ]];
+   then 
+      anio1=$1
+   else 
+      echo "Anio no corresponde "$1
+      exit 1
+   fi
+   if [[ $2 -ge 1 && $2 -le 12 ]];
+   then 
+      mes1=$2
+   else 
+      echo "Mes no corresponde "$2
+   exit 2
+   fi
+   fecha=$1/$mes1/$3
+   echo $fecha
+   if [[ $(date -d$fecha +%w) -eq 0  ]];
+   then
+     linea=$(date -d$fecha +%m,%d,7,%a)
+   else
+     linea=$(date  -d$fecha +%m,%d,%w,%a)
+   #para mac  linea=$(date  -v$fecha +%m,%d,%w,%a)
+   fi
 fi
-cd ..
+
+echo "mes,dia,n_dia_semana,nomdia_semana" > anio${anio1}.csv
+echo $linea >>anio${anio1}.csv
+mv anio${anio1}.csv ../time
 }
 #: Title       : make_tmpdir
 #: Date        : 25/04/2021
@@ -127,6 +93,7 @@ fi
 #: Version     : 1.0  26/04/2021 Actualizacion para IE del 2016
 #: Description : Apatial distribution area emissions
 #: Options     : none
+
 hace_area(){
 if [ $HacerArea -eq 1 ]; then
 echo -e "    \033[1;48m Making Spatial distribution in Area sources \033[0m"
@@ -197,3 +164,84 @@ echo 'Movil Speciation distribution PM2.5'
 echo '** Movil '$MECHA' *****'
 ../bin/spm.exe >> ../movil.log
 }
+#: Title       : check_domain
+#: Date        : 25/04/2021
+#: Author      : "Jose Agustin Garcia Reynoso" <agustin@atmosfera.unam.mx>
+#: Version     : 1.0  26/04/2021 Actualizacion para IE del 2016
+#: Description : Identifies if the domain are for emissions exists
+#: Options     : domain
+check_domain ()
+{
+echo -e "      \033[3;46m   ___  _ ___ _____ ___   \033[0m"
+echo -e "      \033[3;46m  |   \(_) __|_   _| __|  \033[0m"
+echo -e "      \033[3;46m  | |) | | _|  | | | _|   \033[0m"
+echo -e "      \033[3;46m  |___/|_|___| |_| |___|  \033[0m"
+echo -e "      \033[3;46m                          \033[0m"
+cd 01_datos
+existe=0
+if [ -d $1 ]
+then
+   echo "  *** Realizando para "$dominio"   *****"
+else
+   echo -e "\033[1;45m  Dominio "$dominio" no existe en el directorio \033[0m"
+   exit 1
+fi
+cd ..
+}
+#: Title       : hace_namelist
+#: Date        : 25/04/2021
+#: Author      : "Jose Agustin Garcia Reynoso" <agustin@atmosfera.unam.mx>
+#: Version     : 1.0  26/04/2021 Actualizacion para IE del 2016
+#: Description : generates the namelist.nml
+#: Options     : $dia $dia2
+hace_namelist()
+{
+ if [ $1 -eq $2 ]
+ then
+  echo  "  *** Creating namelist.nml for "$dominio"   *****"
+ else
+  echo  "  *** Creating namelist.nml for day "$1"   *****"
+ fi
+cat > namelist_emis.nml << End_Of_File 
+!
+!   Definicion de variables para calculo del Inventario
+!
+&region_nml
+zona ="$dominio"
+!
+! bajio bajio3 cdjuarez   colima    ecacor  ecaim ecaim3
+! guadalajara  jalisco    mexicali  mexico  mexico9
+! monterrey    monterrey3 queretaro tijuana
+/
+&fecha_nml
+! Se indica el dia y el mes a calcular
+! se proporciona el anio
+! month jan =1 to dec=12
+! day in the month (from 1 to 28,30 or 31)
+! anio a modelar validos: 2014 a 2022
+! Si se quiere un archvio de 24 hr periodo=1
+!    o dos archivos de 12 hr peridodo=2
+idia=$dia
+month=$mes
+anio=$nyear
+periodo=$nfile
+/
+!Horario de verano
+&verano_nml
+! .true. o .false. para considerar o no el cambio cuando se
+!  esta en horario de verano
+!
+lsummer = .false.
+/
+! Quimica a utilizar
+! Los mecanismos a usar:
+!  cbm04 cbm05 mozart racm2 radm2 saprc99 saprc07
+!  if saprc07  model= 0 WRF 1 CHIMERE
+&chem_nml
+mecha='$MECHA'
+model=$AQM_SELECT
+/
+End_Of_File
+}
+
+
